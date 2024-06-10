fe180a42bf89a53414795a82934277a70e12b95f
Gerardo Le Roux <126741099+lerock4@users.noreply.github.com>
2024-06-10 00:15:40 -0300

    Implementación de login listo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import router from "./routes/index.js";
 import { connectMongoDB } from "./config/mongoDb.config.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 connectMongoDB();
 
@@ -13,6 +15,15 @@ const ready = console.log("server ready on port " + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://lerouxgerardo:admin123456@cluster0.xee7vf4.mongodb.net/ecommerce",
+    ttl: 15
+    }),
+    secret: "CodigoSecreto",
+    resave: true
+}))
+
 
 app.use("/api",router);
 
